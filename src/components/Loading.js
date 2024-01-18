71444fbe35733e75d49181dfabf5806d4ac99cb9
FordangIT <93567754+FordangIT@users.noreply.github.com>
2024-01-18 17:01:56 +0900

    chore: 부가기능 완료

---
+++ src/components/Loading.js
@@ -0,0 +1,16 @@
+import { LinearProgress } from "@mui/material"
+function Loading() {
+return(
+<div className="flex-col items-center justify-center h-screen"> 
+    <div className="mt-4">
+        <LinearProgress color="secondary"/>
+    </div>
+    <div className="text-center mt-10">
+        AI가 당신의 관상을 확인하고 있습니다.
+    </div>
+</div>
+   
+   
+)
+}
+export default Loading
