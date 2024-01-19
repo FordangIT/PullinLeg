058d420e058ddf782cec7ef168422f94fb5fcace
FordangIT <93567754+FordangIT@users.noreply.github.com>
2024-01-19 14:39:18 +0900

    feat: 기본 주소 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Test1 from "./test1";
+import Main from "./pages/Main";
 const withLayout = (Component) => {
   return (
     <>
@@ -9,6 +10,11 @@ const withLayout = (Component) => {
 }
 
 const router = createBrowserRouter([
+  {
+    path: '/',
+    element: withLayout(<Main />),
+    errorElement: "로딩중"
+  },
   {
     path: '/test1',
     element: withLayout(<Test1 />),
